c06a22e8267a42a2a5dd11c6b9df75fe53c91f5c
albrektsson <emil.albrektsson@nav.no>
2023-07-19 12:19:46 +0200

    Henter stats fra alle teams i SIF

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,7 +12,7 @@ val logger = LoggerFactory.getLogger("Main")
 
 fun main() {
     val applicationContext: ApplicationContext = ApplicationContext.Builder().build()
-    val githubTeam = "k9saksbehandling"
+    val githubTeams = listOf("k9saksbehandling", "omsorgspenger", "dusseldorf", "pleiepenger")
     val githubApiUrl = applicationContext.githubApiUrl
 
     val collectorRegistry = CollectorRegistry()
@@ -24,27 +24,32 @@ fun main() {
 
     val httpClient = applicationContext.httpClient
 
-    val teamRepositories: List<OrgRepositories> = runBlocking {
-        httpClient.get(githubApiUrl + "orgs/navikt/teams/$githubTeam/repos") {
-            parameter("per_page", "100")
-        }.body()
+    val repositories: MutableSet<OrgRepository> = mutableSetOf()
+
+    runBlocking {
+        githubTeams.forEach { team ->
+            val repos: List<OrgRepository> = httpClient.get(githubApiUrl + "orgs/navikt/teams/$team/repos") {
+                parameter("per_page", "100")
+            }.body()
+            repositories.addAll(repos)
+        }
     }
-    logger.info("Found ${teamRepositories.size} repositories for team $githubTeam")
+    logger.info("Found ${repositories.size} repositories for team(s) ${githubTeams.joinToString { it }} }}")
 
     // Filter out archived repos & repos that doesn't belong to the team
-    val filtrertResponse = teamRepositories.filter {
+    val teamRepositories = repositories.filter {
         (it.permissions.push || it.permissions.admin || it.permissions.maintain) && !it.archived
     }.map { it.name }
-    logger.info("Filtered out ${teamRepositories.size - filtrertResponse.size} repositories")
+    logger.info("Filtered out ${repositories.size - teamRepositories.size} repositories")
 
     val openPullRequests: Map<String, Int> = runBlocking {
-        filtrertResponse.associateWith { repository ->
+        teamRepositories.associateWith { repository ->
             // Fetch all open pull requests from repository and return size
             try {
                 httpClient.get(githubApiUrl + "repos/navikt/$repository/pulls") {
                     parameter("per_page", "100")
                     parameter("state", "open")
-                }.body<List<PullRequests>>().size
+                }.body<List<PullRequest>>().size
             } catch (e: Exception) {
                 logger.error("Error fetching open pull requests for repository: $repository, Msg: [${e.message}]")
                 null
@@ -77,16 +82,15 @@ fun main() {
 }
 
 @Serializable
-data class PullRequests(
+data class PullRequest(
     val updated_at: String
 )
 
 @Serializable
-data class OrgRepositories(
+data class OrgRepository(
     val name: String,
     val archived: Boolean,
     val visibility: String,
-    val updated_at: String, // "2011-01-26T19:14:43Z"
     val permissions: Permissions
 )
 
