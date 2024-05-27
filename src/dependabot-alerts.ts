4336ff8a2bc0e744cbff7c4e2599b17694f35e4b
Qadeer Ahmad Khan <qadeer.ahmad.khan@nav.no>
2024-05-27 18:07:01 +0200

    skiller på og high/critical og low-level. Skrive

--- src/dependabot-alerts.ts
+++ src/dependabot-alerts.ts
@@ -1,4 +1,5 @@
 import { octokit } from './octokit'
+import { Endpoints } from '@octokit/types';
 import { hentRepoer } from './common/hentRepoer'
 import { sendSlackMessage } from './common/slackPosting'
 
@@ -7,29 +8,39 @@ let fantAlert = false
 
 for (const repo of repoer) {
     console.log('Henter for repo ' + repo)
-    const dependabotAlerts = await octokit.request('GET /repos/{owner}/{repo}/dependabot/alerts?severity=high', {
+    type DependabotAlertsResponseData = Endpoints['GET /repos/{owner}/{repo}/dependabot/alerts']['response']['data'];
+    const response = await octokit.request('GET /repos/{owner}/{repo}/dependabot/alerts', {
         owner: 'navikt',
         repo: repo,
         state: 'open',
-    })
+    });
 
-    if (dependabotAlerts.data.length > 0) {
+    const dependabotAlerts: DependabotAlertsResponseData = response.data;
+
+
+    if (dependabotAlerts.length > 0) {
         fantAlert = true
+        const highAndcriticals = dependabotAlerts
+            .filter((alert) => { alert.security_vulnerability.severity == 'critical' || alert.security_vulnerability.severity == 'high'})
+        const lows = dependabotAlerts
+            .filter((alert) => { alert.security_vulnerability.severity != 'critical' && alert.security_vulnerability.severity != 'high'})
+
         // eslint-disable-next-line
         const blocks = [] as any[]
         blocks.push({
             type: 'section',
             text: {
                 type: 'mrkdwn',
-                text: `:error: *<https://www.github.com/navikt/${repo}|${repo}>* Har totalt <https://www.github.com/navikt/${repo}/security/dependabot|${dependabotAlerts.data.length} kritiske dependabot alerts>.`,
+                text: `:error: *<https://www.github.com/navikt/${repo}|${repo}>* Har totalt <https://www.github.com/navikt/${repo}/security/dependabot|${highAndcriticals.length} high/critical og ${lows.length} andre dependabot alerts>. ${highAndcriticals.length > 0 ? 'De high/critical:' : ''}`,
             },
         })
-        dependabotAlerts.data.map((alert: any) => {
+
+        highAndcriticals.map((alert) => {
             blocks.push({
                 type: 'section',
                 text: {
                     type: 'mrkdwn',
-                    text: `*<${alert.html_url}|${alert.security_advisory.summary}>*`,
+                    text: `*<${alert.html_url}|${alert.security_advisory.severity}: ${alert.security_advisory.summary} }>*`,
                 },
             })
         })
