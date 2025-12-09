b2637e64d7a1deca6e3512dd641ddcdc474d33be
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2025-12-09 16:25:00 +0100

    fjerner funksjonalitet for å sende dependabot alerts og stale PR til #sif-sikkerhetsvarsel på slack det ikke er i bruk og krever PAT

--- src/octokit.ts
+++
@@ -1,8 +0,0 @@
-import { Octokit } from '@octokit/rest'
-
-if (!process.env['GITHUB_TOKEN']) {
-    throw Error('Missing env GITHUB_TOKEN')
-}
-export const octokit = new Octokit({
-    auth: process.env['GITHUB_TOKEN'],
-})
